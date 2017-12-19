9d4d778883fb62809dbd9b5f7f0afcd887ed37de
Steffen H <haucks@live.de>
2017-12-19 21:19:15 +0100

    Revamp

--- ShinyChance.py
+++ ShinyChance.py
@@ -1,40 +1,36 @@
+#After Gen 5 the Chance to obtain a shiny Pokemon doubled
+
 gen1 = 8191/8192
 gen2 = 4095/4096
+banner = ("\n------------------------------------")
+print("Shiny Pokemon Probability Calculator",banner)
+while(True):
+        def GenAsk():
+                print("Do want to to calcute:\n\n1. Pre Gen 5 1:8192\nor\n2. Post Gen 5 1:4096?")
+                cl = int(input(">>>>>"))
+                if cl == 1:
+                        x = gen1
+                        return x
+                elif cl == 2:
+                        x = gen2
+                        return x
+                else:
+                        print("Choose 1 or 2!")
+                        GenAsk()
+        def Calc():
+                print()
+                print("Do you want your output as\n\n1. A List\nor\n2. A Single Output?")
+                ch = int(input(">>>>>"))
+                #Put Loop here
+                if ch == 1:
+                        
+        GenAsk()
+        
+        
+                
+        
 
-def ShinyRoll(x = gen1):
-	Chance=0
-	try:
-	    n=int(input("Type the number of SRs: "))
-	except:
-	    print("You screwed up.")
-
-	Chance=(1-((x)**n))*100
-	print("Encounter: ",n,"  Shiny Chance: ",Chance,"%")
-
-def ShinyList():
-	Chance=0
-	i=1
-	try:
-	    n=int(input("Type the number of cycles: "))
-	except:
-	    print("You screwed up.")
-
-	while(i <= n):
-	    Chance=(1-((gen1)**i))*100
-	    print("Encounter: ",i,"  Shiny Chance: ",Chance,"%")
-	    i+=1
-
-def ShinyRollNew():
-        Chance=0
-        ans = False
-        while(ans == False):
-                try:
-                        n=int(input("Type the number of SRs: "))
-                        ans = True
-                except:
-                        print('Nope.')
-
-        Chance=(1-((gen2)**n))
-        print("Encounter: ",n," Shiny Chance: ",Chance,"%")
-
-
+#Chance = (1-((x)**n)*100)
+#n = Cycles
+#replace n with i for list
+#Good night
