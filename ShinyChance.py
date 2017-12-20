ea5f49c99f0025de9079cda65189c46c5a560aeb
Steffen H <haucks@live.de>
2017-12-20 18:29:11 +0100

    Revamped Version

--- ShinyChance.py
+++ ShinyChance.py
@@ -4,28 +4,44 @@ gen1 = 8191/8192
 gen2 = 4095/4096
 banner = ("\n------------------------------------")
 print("Shiny Pokemon Probability Calculator",banner)
-while(True):
-        def GenAsk():
-                print("Do want to to calcute:\n\n1. Pre Gen 5 1:8192\nor\n2. Post Gen 5 1:4096?")
-                cl = int(input(">>>>>"))
-                if cl == 1:
-                        x = gen1
-                        return x
-                elif cl == 2:
-                        x = gen2
-                        return x
-                else:
-                        print("Choose 1 or 2!")
+def GenAsk():
+        print("Do want to to calcute:\n\n1. Pre Gen 5 1:8192\nor\n2. Post Gen 5 1:4096?")
+        cl = int(input(">>>>>"))
+        if cl == 1:
+                x = gen1
+        elif cl == 2:
+                x = gen2
+        else:
+                print("Choose 1 or 2!")
+                GenAsk()
+        print()
+        print("Do you want your output as\n\n1. A List\nor\n2. A Single Output?")
+        ch = int(input(">>>>>"))
+        if ch == 2:
+                try:
+                        n = int(input("Type the number of Soft Resets: "))
+                except:
                         GenAsk()
-        def Calc():
-                print()
-                print("Do you want your output as\n\n1. A List\nor\n2. A Single Output?")
-                ch = int(input(">>>>>"))
-                #Put Loop here
-                if ch == 1:
+                Chance = ((1-(x**n))*100)
+                print("SRs: ",n," Shinyprobability: ",Chance,"%")
+                GenAsk()
+        elif ch == 1:
+                try:
+                        n = int(input("Type the number of SR Cycles: "))
+                except:
+                        GenAsk()
+                i = 1
+                while(i<=n):
+                        Chance = ((1-(x**i))*100)
+                        print("SRs: ",i," Shinyprobability: ",Chance,"%")
+                        i+=1
+                GenAsk()
+        else:
+                GenAsk()
                         
-        GenAsk()
-        
+
+                
+GenAsk()
         
                 
         
